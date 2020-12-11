cfa610ec9b6537e837879e5a0dd6a88857d56b3c
cbh <47790293@qq.com>
2020-12-11 09:32:38 +0800

    Fistr

---
+++ CenManagementSystem/App_Start/Filters/LogoinFilterAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CenManagementSystem.App_Start
+{
+    public class LogoinFilterAttribute : ActionFilterAttribute
+    {
+        public bool IsCheck { get; set; }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            
+                if (filterContext.HttpContext.Session["AdminName"] == null)
+                {
+                    filterContext.HttpContext.Response.Redirect("/Logoin/Index");
+               }
+            
+        }
+    }
+}
---
+++ CenManagementSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CenManagementSystem.MvcApplication" Language="C#" %>
